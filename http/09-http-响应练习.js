ba3d7a4b38441e637ba09d4e6e7695f644e2001a
chen <18551681083@163.com>
2024-01-22 13:50:48 +0800

    响应练习，多文件

--- http/09-http-响应练习.js
+++ http/09-http-响应练习.js
@@ -5,16 +5,21 @@ const path = require("path");
 
 // 创建服务
 const server = http.createServer((req, res) => {
-    fs.readFile(path.resolve(__dirname, "table.html"), (err, data) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end();
-        }
-        else {
-            res.setHeader("Content-Type", "text/html");
-            res.end(data);
-        }
-    });
+    const { pathname } = new URL(req.url, "http://127.0.0.1:8080");
+    console.log(pathname);
+    if (pathname == "/") {
+        res.setHeader("Content-type", "text/html");
+        fs.readFile(path.resolve(__dirname, "table.html"), (err, data) => res.end(data));
+    } else if (pathname == "/table.css") {
+        res.setHeader("Content-type", "text/css");
+        fs.readFile(path.resolve(__dirname, "table.css"), (err, data) => res.end(data));
+    } else if (pathname == "/table.js") {
+        res.setHeader("Content-type", "text/javascript");
+        fs.readFile(path.resolve(__dirname, "table.js"), (err, data) => res.end(data));
+    } else {
+        res.statusCode = 404;
+        res.end("<h1>Not Found</h1>");
+    }
 });
 
 // 监听端口

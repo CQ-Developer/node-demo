0ce793fbac1c0134c8ed7ad479a6518d329a953a
chen <18551681083@163.com>
2024-01-22 16:39:51 +0800

    文件夹的导入规则

---
+++ module/03-暴露文件夹/01-main属性/main.js
@@ -0,0 +1,4 @@
+// 导入
+// 如果目标是一个文件夹，首先检测文件夹下的package.json中的main属性对应的文件
+const m = require("./module");
+console.log(m);
---
+++ module/03-暴露文件夹/02-index.js/main.js
@@ -0,0 +1,4 @@
+// 导入文件夹
+// 如果main属性不存在或package.json不存在，则尝试导入index.js或index.json
+const m = require("./module");
+console.log(m);
